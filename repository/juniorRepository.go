fe710c9a9ab198d4e2de2ed8249dca851e540ae9
Ateto1202 <tonysu1204@gmail.com>
2024-08-19 10:22:08 +0800

    Update repositories of junior and senior

--- repository/juniorRepository.go
+++ repository/juniorRepository.go
@@ -1,4 +1,34 @@
 package repository
 
+import (
+	"fmt"
+
+	"github.com/we-we-Web/draw-lots-backend/model"
+	"gorm.io/gorm"
+)
+
 type JuniorRepository struct {
+	Database *gorm.DB
+}
+
+func NewJuniorRepo(db *gorm.DB) *JuniorRepository {
+	return &JuniorRepository{Database: db}
+}
+
+func (repo *JuniorRepository) CreateJunior(junior *model.Junior) error {
+	if err := repo.Database.Create(&junior).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *JuniorRepository) GetJunior(id string) *model.Junior {
+	var junior model.Junior
+
+	if err := repo.Database.First(&junior, "student_number = ?", id).Error; err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Junior found:", junior)
+	}
+	return &junior
 }
--- repository/seniorRepository.go
+++ repository/seniorRepository.go
@@ -1,4 +1,11 @@
 package repository
 
+import "gorm.io/gorm"
+
 type SeniorRepository struct {
+	Database *gorm.DB
+}
+
+func NewSeniorRepo(db *gorm.DB) *SeniorRepository {
+	return &SeniorRepository{Database: db}
 }
